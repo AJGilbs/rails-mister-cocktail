a67cfed671cca3d807cdfb6eb5744cb0d5feb65f
Andrew Gilbert <andrew-gilbert1@hotmail.co.uk>
2017-11-09 13:45:58 +0000

    controllers are correct

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+
+  # get 'doses/create'
+
+  # get 'doses/destroy'
+
+  # get 'cocktails/index'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/create'
+
+  resources :cocktails, only: [ :new, :create, :index, :show] do
+    resources :doses, only: [ :new, :create ]
+  end
+  resources :doses, only: [ :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+parsed_ingredients = open(url).read
+
+ingredients = JSON.parse(parsed_ingredients)
+
+ingredients["drinks"].each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
+
