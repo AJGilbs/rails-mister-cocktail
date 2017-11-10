7358111256ccd989e7f76636735487af36214eb9
Andrew Gilbert <andrew-gilbert1@hotmail.co.uk>
2017-11-10 12:39:40 +0000

    prepre for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  # get 'doses/new'
 
-  # get 'doses/create'
-
-  # get 'doses/destroy'
-
-  # get 'cocktails/index'
-
-  # get 'cocktails/show'
-
-  # get 'cocktails/new'
-
-  # get 'cocktails/create'
+ root to: 'cocktails#index'
 
   resources :cocktails, only: [ :new, :create, :index, :show] do
     resources :doses, only: [ :new, :create ]
